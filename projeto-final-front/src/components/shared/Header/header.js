64ae7715b54c68fdc6ad93ad959efb9fba364832
Valdenir Silva Brito <59507605+ValdenirSBrito@users.noreply.github.com>
2021-11-21 20:57:58 -0400

    Update header.js

--- projeto-final-front/src/components/shared/Header/header.js
+++ projeto-final-front/src/components/shared/Header/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const Header = () => {
   return (
-    <nav className="navbar  navbar-expand-lg navbar-light bg-primary w-100">
+    <nav className="navbar  navbar-expand-lg navbar-light  w-100">
       <div className="container">
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -10,19 +10,21 @@ const Header = () => {
         <Link className="navbar-brand" to="/">
           Lista de Tarefas
         </Link>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" to="/">
+        <div className="ms-auto">
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" to="/">
                 Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/cadastro">
-                Cadastro
-              </Link>
-            </li>
-          </ul>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/cadastro">
+                 Cadastro
+                </Link>
+              </li>
+           </ul>
+          </div>
         </div>
       </div>
     </nav>
